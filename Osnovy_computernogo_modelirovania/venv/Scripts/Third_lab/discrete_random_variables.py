f340a84f905aa489cc3850a289b6c647ba37dce6
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-03-31 22:38:53 +0300

    3 лаба

--- Osnovy_computernogo_modelirovania/venv/Scripts/Third_lab/discrete_random_variables.py
+++ Osnovy_computernogo_modelirovania/venv/Scripts/Third_lab/discrete_random_variables.py
@@ -1,30 +1,16 @@
-def form_polynom_first_order(a0, a1):
-    def polynom(x):
-        return a0 + a1*x
-    return polynom
+import matplotlib.pyplot as plt
+import random
+import numpy as np
 
 
-def form_polynom_third_order(a0, a1, a2, a3):
-    def polynom(x):
-        return a0 + a1*x + a2*pow(x, 2) + a3*pow(x, 3)
-    return polynom
-
-
-def form_logistics_curve(k, a, b):
-    def formula(x):
-        return 1/(k+a*pow(b,x))
-    return formula
-
-
-
-def fourth_algorithm(N, a0=1, q=5 ** 17, M=2 ** 42):
+def congruent_method(N, a0=1, q=5 ** 17, M=2 ** 42):
     values = [a0]
     for i in range(N):
         values.append((q * values[-1] % M))
-    return list(map(lambda x: x/10000, values))
+    return list(map(lambda x: round(x / 10 ** len(str(x)),4), values))
 
 
-def third_algorithm(gamma0, N, k = None):
+def method_square(gamma0, N, k = None):
     if k == None:
         k = len(str(gamma0))
     values = [gamma0]
@@ -33,9 +19,37 @@ def third_algorithm(gamma0, N, k = None):
     return list(map(lambda x: x/(10**k), values))
 
 
-def first_algorithm():
-    pass
+def create_hist(data, title):
+    n, bin, patches = plt.hist(data, bins=len(data))
+    plt.xlabel('Значение')
+    plt.ylabel('Частота встречи')
+    x1, x2, y1, y2 = plt.axis()
+    plt.axis((x1, x2, 0, 10))
+    plt.title(title)
+    plt.show()
+
+
+def composition_method(y0, q, N):
+    n = len(str(y0))
+    values = [y0]
+    values_res = []
+    for _ in range(N):
+        mult = values[-1] * q
+        values_res.append(int(mult / 10 ** (n / 2)) % 10 ** n)
+        values.append(mult % 10 ** n)
+    return list(map(lambda x: x / 10 ** len(str(x)), values_res))
 
 
 if __name__ == '__main__':
-    print(third_algorithm(7153, 6))
+    square_result = method_square(2134, 1000)
+    square_result_second = method_square(3167, 1000)
+    square_result_third = method_square(3485, 1000)
+    congruent_result = congruent_method(1000)
+    composition_result = composition_method(3729, 5167, 1000)
+
+    create_hist(square_result, 'Метод квадратов (x0 = 2134)')
+    create_hist(square_result_second, 'Метод квадратов (x0 = 3167)')
+    create_hist(square_result_third, 'Метод произведений (x0 = 3485)')
+
+    create_hist(congruent_result, 'Конгруэнтный метод')
+    create_hist(composition_result, 'Метод произведений')
