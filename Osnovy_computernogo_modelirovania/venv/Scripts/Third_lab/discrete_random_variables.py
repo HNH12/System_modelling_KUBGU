648952aec0e610533b39da9dbceececf1dc87ed3
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-01 20:05:06 +0300

    3 лаба. Тестирования

--- Osnovy_computernogo_modelirovania/venv/Scripts/Third_lab/discrete_random_variables.py
+++ Osnovy_computernogo_modelirovania/venv/Scripts/Third_lab/discrete_random_variables.py
@@ -3,32 +3,6 @@ import random
 import numpy as np
 
 
-def congruent_method(N, a0=1, q=5 ** 17, M=2 ** 42):
-    values = [a0]
-    for i in range(N):
-        values.append((q * values[-1] % M))
-    return list(map(lambda x: round(x / 10 ** len(str(x)),4), values))
-
-
-def method_square(gamma0, N, k = None):
-    if k == None:
-        k = len(str(gamma0))
-    values = [gamma0]
-    for i in range(N):
-        values.append(int(pow(values[-1], 2) / pow(10, k / 2)) % pow(10, k))
-    return list(map(lambda x: x/(10**k), values))
-
-
-def create_hist(data, title):
-    n, bin, patches = plt.hist(data, bins=len(data))
-    plt.xlabel('Значение')
-    plt.ylabel('Частота встречи')
-    x1, x2, y1, y2 = plt.axis()
-    plt.axis((x1, x2, 0, 10))
-    plt.title(title)
-    plt.show()
-
-
 def composition_method(y0, q, N):
     n = len(str(y0))
     values = [y0]
@@ -40,16 +14,98 @@ def composition_method(y0, q, N):
     return list(map(lambda x: x / 10 ** len(str(x)), values_res))
 
 
+def square_method(first_digit, N):
+    n = len(str(first_digit))
+    values = [first_digit]
+    for _ in range(N):
+        values.append(int(values[-1] ** 2 / 10 ** (n/2)) % 10 ** n)
+    return list(map(lambda x: x / 10 ** len(str(x)), values))
+
+
+def comparison_method(N, a0=1, q=5 ** 17, M=2 ** 42):
+    values = [a0]
+    for _ in range(N):
+        values.append(q * values[-1] % M)
+    return list(map(lambda x: x / 10 ** len(str(x)), values))
+
+
+def create_hist(data):
+    n, bin, patches = plt.hist(data, bins=len(data))
+    return n, patches
+
+
+def period(sequence):
+    y = sequence[-1]
+    periods = []
+    count = 0
+    for el in sequence:
+        if el == y:
+            periods.append(count)
+            count = 0
+        else:
+            count += 1
+    return max(periods)
+
+
+def frequency_test(sequence):
+    count = 0
+    for el in sequence:
+        if el > 0.2113 and el < 0.7887:
+            count += 1
+    return count / len(sequence) * 100
+
+
+def uniformity_test(sequence):
+    x_squad = 0
+    N = sum(sequence)
+    for el in sequence:
+        p = 1/len(sequence)
+        x_squad += pow((el - p * N),2)/(p*N)
+    return x_squad
+
+
 if __name__ == '__main__':
-    square_result = method_square(2134, 1000)
-    square_result_second = method_square(3167, 1000)
-    square_result_third = method_square(3485, 1000)
-    congruent_result = congruent_method(1000)
-    composition_result = composition_method(3729, 5167, 1000)
+    sequence1 = np.random.uniform(0, 1, 500)
+    period1 = period(sequence1)
+    print()
+    print('Uniform (встроенная библиотека):')
+    print('Частотный тест ', frequency_test(sequence1))
+    print('Период последовательности: ', period1)
+    print('Равномерный тест: ', uniformity_test(sequence1))
 
-    create_hist(square_result, 'Метод квадратов (x0 = 2134)')
-    create_hist(square_result_second, 'Метод квадратов (x0 = 3167)')
-    create_hist(square_result_third, 'Метод произведений (x0 = 3485)')
+    sequence2 = composition_method(3729, 5167, 500)
+    period2 = period(sequence2)
+    print()
+    print('Метод произведений:')
+    print('Частотный тест ', frequency_test(sequence2))
+    print('Период последовательности: ', period2)
+    print('Равномерный тест: ', uniformity_test(sequence2))
 
-    create_hist(congruent_result, 'Конгруэнтный метод')
-    create_hist(composition_result, 'Метод произведений')
+    sequence3 = square_method(7153, 500)
+    period3 = period(sequence3)
+    print()
+    print('Метод квадратов:')
+    print('Частотный тест ', frequency_test(sequence3))
+    print('Период последовательности: ', period3)
+    print('Равномерный тест: ', uniformity_test(sequence3))
+
+    sequence4 = comparison_method(500, 1357, 1357, 9689)
+    period4 = period(sequence4)
+    print()
+    print('Конгруэнтный метод')
+    print('Частотный тест ', frequency_test(sequence4))
+    print('Период последовательности: ', period4)
+    print('Равномерный тест: ', uniformity_test(sequence4))
+
+    create_hist(sequence1)
+    plt.title('Uniform')
+    plt.show()
+    create_hist(sequence2)
+    plt.title('Произведений')
+    plt.show()
+    create_hist(sequence3)
+    plt.title('Квадратов')
+    plt.show()
+    create_hist(sequence4)
+    plt.title('Конгруэнтный')
+    plt.show()
