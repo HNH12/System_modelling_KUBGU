42de980eec781353680e0bca716d307740601f45
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-05-05 16:31:55 +0300

    Финальная версия

--- Osnovy_computernogo_modelirovania/venv/Scripts/Fifth_lab/main_task.py
+++ Osnovy_computernogo_modelirovania/venv/Scripts/Fifth_lab/main_task.py
@@ -24,7 +24,24 @@ def read_params(file_path):
                     minimum_machine_setup_time = float(line[33:-2])
                 elif 'Максимальное время наладки станка' in line:
                     maximum_machine_setup_time = float(line[34:-2])
-                    print(maximum_machine_setup_time)
+                elif 'Математическое ожидание времени выполнения задания' in line:
+                    mathematical_expectation_task_completion = float(line[51:-2])
+                elif 'Среднеквадратичное отклонение времени выполнения задания' in line:
+                    standard_deviation_task_completion = float(line[57:-2])
+                elif 'Математическое ожидание поломки' in line:
+                    mathematical_expectation_breakdowns = float(line[32:-2])
+                elif 'Среднеквадратичное отклонение поломки' in line:
+                    standard_deviation_breakdowns = float(line[38:-2])
+                elif 'Минимальное время устранения поломки' in line:
+                    minimum_machine_fixing_breakdown = float(line[37:-2])
+                elif 'Максимальное время устранения поломки' in line:
+                    maximum_machine_fixing_breakdown = float(line[38:-2])
+                else:
+                    count_details = int(line[19:-1])
+
+    return minimum_machine_setup_time, maximum_machine_setup_time, mathematical_expectation_task_completion, \
+           standard_deviation_task_completion,mathematical_expectation_breakdowns,standard_deviation_breakdowns, \
+           minimum_machine_fixing_breakdown, maximum_machine_fixing_breakdown, count_details
 
 
 def modelling(minimum_machine_setup_time, maximum_machine_setup_time, mathematical_expectation_task_completion,
@@ -83,8 +100,7 @@ def modelling(minimum_machine_setup_time, maximum_machine_setup_time, mathematic
 
 
 def main():
-    print(read_params('params'))
-    # modelling()
+    modelling(*read_params('params'))
     # list = [random.expovariate(1) for i in range(0,500)]
     # print(sum(list)/500)
 
