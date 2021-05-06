7247bc10fe8d4f384dc63f95decf5d7c9a814b7a
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-05-06 17:45:19 +0300

    Update main_task.py

--- Osnovy_computernogo_modelirovania/venv/Scripts/Sixth_task/main_task.py
+++ Osnovy_computernogo_modelirovania/venv/Scripts/Sixth_task/main_task.py
@@ -3,11 +3,10 @@ import random
 
 
 def modelling():
-    count_evm = 2
     all_time = 2
     task_receipt_period = 2
     registration_time = 12
-    number_tasks_received_during_processing = registration_time/task_receipt_period
+    number_tasks_received_during_processing = registration_time / task_receipt_period
     error_probability = 0.7
     time_resolve_error = 3
     time_complete = 10
@@ -25,45 +24,85 @@ def modelling():
     count_task = 100
     counter = 1
     queue_on_hold = 1
+    current_task = 0
 
-    while counter <= count_task and queue_on_hold != 0:
-        print('В очереди на регистрацию: ')
+    while (counter <= count_task and queue_on_hold) or check_error_second_evm or check_error_first_evm != 0:
+        # print(counter, queue_on_hold)
+        # print('В очереди на регистрацию: ')
+        # print('Время', all_time, 'минут')
+        if workload_first_evm > workload_second_evm and check_registration and check_error_second_evm:
+            workload_second_evm += 1
+            if queue_on_hold == 0:
+                all_time += 10
+            print('Выполнение задания на второй ЭВМ')
+            if random.random() < error_probability and not check_error_second_evm:
+                check_error_second_evm = True
+                check_error_second_complete = True
+                all_time += 3
+                workload_second_evm -= 1
+                print('Ошибка!')
+                current_task -= 1
+                print('Исправление ошибки')
+            else:
+                workload_second_evm -= 1
+                check_error_second_complete = False
+                check_error_second_evm = False
+        elif check_registration and check_error_first_evm:
+            workload_first_evm += 1
+            if queue_on_hold == 0:
+                all_time += 10
+            print('Выполнение задания на первой ЭВМ')
+            if random.random() < error_probability and not check_error_first_evm:
+                check_error_first_evm = True
+                workload_first_evm -= 1
+                all_time += 3
+                print('Ошибка!')
+                current_task -= 1
+                print('Исправление ошибки')
+            else:
+                workload_first_evm -= 1
+                check_error_first_complete = False
+                check_error_first_evm = False
+                print()
+        else:
+            workload_second_evm += 1
+            if queue_on_hold == 0:
+                all_time += 10
+            print('Выполнение задания на второй ЭВМ')
+            if random.random() < error_probability and not check_error_second_evm:
+                check_error_second_evm = True
+                check_error_second_complete = True
+                all_time += 3
+                workload_second_evm -= 1
+                print('Ошибка!')
+                current_task -= 1
+                print('Исправление ошибки')
+            else:
+                workload_second_evm -= 1
+                check_error_second_complete = False
+                check_error_second_evm = False
+
+        current_task += 1
+        if check_error_first_evm or check_error_second_evm:
+            print('Повтор выполнения', current_task, 'задания')
+        else:
+            print('Текущее задание', current_task)
         if not check_error_first_evm and not check_error_second_evm:
             if (counter + 6 > count_task) and counter < count_task:
-                counter += count_task - counter
-                queue_on_hold += count_task - counter
+                differ = count_task - counter
+                counter += differ
+                queue_on_hold += differ
             elif counter < count_task:
                 counter += 6
                 queue_on_hold += 6
             if queue_on_hold != 0:
                 queue_on_hold -= 1
                 all_time += registration_time
-
-        if workload_first_evm > workload_second_evm:
-            workload_second_evm += 1
-            all_time += 10
-            if random.random() < error_probability and not check_error_second_evm:
-                check_error_second_evm = True
-                check_error_second_complete = True
-                all_time += 3
-                workload_second_evm -= 1
-            else:
-                workload_second_evm -= 1
-                check_error_second_complete = False
-                check_error_second_evm = False
-        else:
-            workload_first_evm += 1
-            all_time += 10
-            if random.random() < error_probability and not check_error_first_evm:
-                check_error_first_evm = True
-                workload_first_evm -= 1
-                all_time += 3
-            else:
-                workload_first_evm -= 1
-                check_error_first_complete = False
-                check_error_first_evm = False
-
-    print(all_time/60)
+                check_registration = True
+                print('Регистрация')
+        # if not check_error_first_evm or not check_error_second_evm:
+    print()
+    print(all_time / 60)
 
 
 def main():
